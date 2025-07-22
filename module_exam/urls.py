e5ad5ca6baf62d718883ac220315233ab485a24e
HOSEINMONJEZY83 <HOSEINMONJEZY1383@gmail.com>
2025-07-22 16:00:45 +0330

    Add authentication and teacher dashboard routes

--- module_exam/urls.py
+++ module_exam/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login',views.custom_login, name='login'),
+    path('logout',views.user_logout, name='logout'),
+    path('teacher/dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
     path('exam/<int:exam_id>/', views.take_exam, name='take_exam'),
 ]
