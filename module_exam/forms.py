c9ff7d3de9ed109cc8706a463fce2710fb8e7083
HOSEINMONJEZY83 <HOSEINMONJEZY1383@gmail.com>
2025-07-22 17:32:52 +0330

    Add teachers field to registration form with validation for student role

--- module_exam/forms.py
+++ module_exam/forms.py
@@ -4,28 +4,33 @@ from .models import Question, Exam , User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    teachers = forms.ModelMultipleChoiceField(
+        queryset=User.objects.filter(role='teacher'),
+        widget=forms.SelectMultiple(attrs={'class': 'form-control'}),
+        required=False,
+        label='Select Teachers'
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'role', 'password1', 'password2']
-        widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}),
-            'role': forms.Select(attrs={'class': 'form-control'}),
-        }
+        fields = ['username', 'role', 'teachers', 'password1', 'password2']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['password1'].widget = forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Enter password'
-        })
-        self.fields['password2'].widget = forms.PasswordInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Repeat password'
-        })
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter username'})
+        self.fields['role'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Enter password'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Repeat password'})
+        self.fields['teachers'].widget.attrs.update({'class': 'form-control'})
 
-        self.fields['password1'].help_text = ''
-        self.fields['password2'].help_text = ''
+    def clean(self):
+        cleaned_data = super().clean()
+        role = cleaned_data.get('role')
+        teachers = cleaned_data.get('teachers')
+        if role == 'student' and not teachers:
+            raise forms.ValidationError('Students must select at least one teacher.')
+        return cleaned_data
         
 
 class ExamForm(forms.ModelForm):
